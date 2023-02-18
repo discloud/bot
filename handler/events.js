fb2dcd91b17091295e11a843fa86b78a7f8417ff
teilorr <carloscordeirosousa@gmail.com>
2023-02-18 01:57:35 -0300

    Delete old project

--- handler/events.js
+++
@@ -1,8 +0,0 @@
-const { readdirSync } = require('fs')
-
-module.exports = () => {
-    const events = readdirSync('./events/')
-    for (const event of events)
-        require(`../events/${event}`)
-    return console.log('events loaded')
-}
